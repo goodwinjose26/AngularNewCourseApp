033589d1305ffc3cdd4f5bd71ee79e59c7e59f6a
goodwinjose26 <josegoodwin22@gmail.com>
2022-12-06 19:30:27 +0530

    view

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -11,4 +11,8 @@ export class ApiService {
   {
     return this.http.post("http://localhost:8080/add",datatosend)
   }
+  viewcourse =()=>
+  {
+    return this.http.get("http://localhost:8080/view")
+  }
 }
